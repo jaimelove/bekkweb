4b03c1ac019ca1ff87093224b3fa36b844b6866d
Jaime <jaimelovehansen@gmail.com>
2018-09-05 19:31:03 +0200

    added index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Heihei</title>
+  </head>
+  <body>
+    <h1>STORT eln idk</h1>
+    <p>teksteteksteeee</p>
+  </body>
+</html>
